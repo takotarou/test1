fd1f3bf46718444f66e71da110ee2b6e41203b77
EC2 Default User <ec2-user@ip-172-31-38-18.ap-southeast-1.compute.internal>
2021-07-07 14:57:55 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'questions#index'
+  resources :questions do
+    resources :answers
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Question.create(id: 1,name: 'Test name 1', title: 'Test Title 1', content: 'Test content 1')
+Question.create(id: 2,name: 'Test name 2', title: 'Test Title 2', content: 'Test content 2')
+Question.create(id: 3,name: 'Test name 3', title: 'Test Title 3', content: 'Test content 3')
